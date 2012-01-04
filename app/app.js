ab1c40e9ccc5cd0e5b5c750b10260c1a99cf0744
Adrien Olczak <adrien.olczak@gmail.com>
2012-01-04 12:19:26 +0100

    added app icon

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 Relief1 = new Ext.Application({
 	name: 'Relief1',
+	icon: 'app_icon9.png',
+	glossOnIcon: false,	
+	tabletStartupScreen: '',
+	phoneStartupScreen: '',
 	
 	launch: function() {
 		this.views.Viewport = new this.views.Viewport();
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>Relief1</title>
+	
+	<!-- Sencha touch CSS -->
+	<link rel="stylesheet" href="lib/resources/css-debug/sencha-touch.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	<link rel="stylesheet" href="stylesheets/main.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	<link rel="stylesheet" href="stylesheets/feed.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	
+	<!-- Sencha touch JS -->
+	<script src="lib/sencha-touch-debug.js" type="text/javascript" charset="utf-8"></script>
+	
+	<!-- GoogleMap API -->
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	
+	<!-- Application JS - App, Models, Stores, Views, Controllers (in that order) -->
+	<script src="app/app.js" type="text/javascript" charset="utf-8"></script>
+	<script src="data/DonationList_data.js" type="text/javascript" charset="utf-8"></script>
+	
+	 
+	<script src="app/views/Viewport.js" type="text/javascript" charset="utf-8"></script>	
+	<script src="app/views/feed.js" type="text/javascript" charset="utf-8"></script>
+	<script src="app/views/map.js" type="text/javascript" charset="utf-8"></script>
+	<script src="app/views/profil.js" type="text/javascript" charset="utf-8"></script>
+	
+</head>
+<body></body>
+</html>
